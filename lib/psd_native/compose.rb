48cd26f3f1537fe6f707f14c2c534f7781a12455
Micka33 <mickael@beingenious.com>
2014-06-13 13:10:15 +0200

    forgot unless, lol

--- lib/psd_native/compose.rb
+++ lib/psd_native/compose.rb
@@ -19,7 +19,7 @@ class PSD
     def do_blend(blend, fg, bg, opts={})
       File.open('/Users/cassymickael/Desktop/micka.tmp.text', 'a') { |file| file.write("blend:#{blend.to_s}, opts:#{opts.to_s}\n") }
       DEFAULT_OPTS[:opacity] = opts.to_i if opts.is_a? Integer 
-      opts = DEFAULT_OPTS.merge(opts)
+      opts = DEFAULT_OPTS.merge(opts) unless opts.is_a? Integer 
       PSDNative::Compose.send(blend, fg, bg, opts)
     end
   end
